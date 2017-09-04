21a5983601cb18e2139bae7b2adf5a9f74c65d90
KaiserChan <chankaiserkk@gmail.com>
2017-09-04 17:59:37 -0400

    Updated photogur ... completed challenge 2 - added method to check picture creation year

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,12 @@
 class PicturesController < ApplicationController
 
   def index
-    @pictures = Picture.all
+    @most_recent_pictures = Picture.most_recent_five
   end
+
+  # def index
+  #   @pictures = Picture.all
+  # end
 
   def show
     @picture = Picture.find(params[:id])
